cce9c5f3fad873731886e9e4263f60f8911bf68f
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-06-09 06:25:19 +0900

    edit_devise_view
    ホームページ、ユーザー管理画面を装飾

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 
 Rails.application.routes.draw do
 
+  devise_for :users
+
   # ホームページ
   root "users#index"
+
+  # ユーザー関連パス
+  resources :users, only: [:show]
+
+  # 分野関連パス
+  resources :fields, only: [:index]
 
 end
